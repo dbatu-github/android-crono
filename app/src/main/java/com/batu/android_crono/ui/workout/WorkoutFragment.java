3f58950b27101ac4b82b82913726f5122bee980c
david <davidbatuecas@gmail.com>
2020-05-17 22:15:54 +0200

    Renombrado de Home a Workout

--- app/src/main/java/com/batu/android_crono/ui/workout/WorkoutFragment.java
+++ app/src/main/java/com/batu/android_crono/ui/workout/WorkoutFragment.java
@@ -1,4 +1,4 @@
-package com.batu.android_crono.ui.home;
+package com.batu.android_crono.ui.workout;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,20 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.batu.android_crono.R;
 
-public class HomeFragment extends Fragment {
+public class WorkoutFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private WorkoutViewModel workoutViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        workoutViewModel = new ViewModelProvider(this).get(WorkoutViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_workout, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        workoutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/batu/android_crono/ui/workout/WorkoutViewModel.java
+++ app/src/main/java/com/batu/android_crono/ui/workout/WorkoutViewModel.java
@@ -1,14 +1,14 @@
-package com.batu.android_crono.ui.home;
+package com.batu.android_crono.ui.workout;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class WorkoutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public WorkoutViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
